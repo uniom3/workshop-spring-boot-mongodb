71c8a297572c37d3717a06f3b5517590088ba28b
uniom3 <48806717+uniom3@users.noreply.github.com>
2020-01-24 11:08:50 -0300

    Obtendo usuário por Id

--- src/main/java/com/mendonca/workshopmongo/services/UserService.java
+++ src/main/java/com/mendonca/workshopmongo/services/UserService.java
@@ -1,20 +1,28 @@
 package com.mendonca.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.mendonca.workshopmongo.domain.User;
 import com.mendonca.workshopmongo.repository.UserRepository;
+import com.mendonca.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
---
+++ src/main/java/com/mendonca/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.mendonca.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	
+
+}
