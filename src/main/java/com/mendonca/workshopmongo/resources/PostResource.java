c4a1c8de52ca4a5a03f6a764a4463a111f4f330b
uniom3 <48806717+uniom3@users.noreply.github.com>
2020-08-30 20:00:29 -0300

    Consulta simples com query methods

--- src/main/java/com/mendonca/workshopmongo/resources/PostResource.java
+++ src/main/java/com/mendonca/workshopmongo/resources/PostResource.java
@@ -1,13 +1,18 @@
 package com.mendonca.workshopmongo.resources;
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mendonca.workshopmongo.domain.Post;
+import com.mendonca.workshopmongo.resources.exception.URL;
 import com.mendonca.workshopmongo.services.PostService;
 
 @RestController
@@ -23,4 +28,11 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
--- src/main/java/com/mendonca/workshopmongo/services/PostService.java
+++ src/main/java/com/mendonca/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.mendonca.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,8 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 	
 }
